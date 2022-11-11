e236a212d40ab1a06f86cfb8d70351cee64416ee
virendra1485 <virendrasinghrawat1485@gmail.com>
2022-11-11 15:27:09 +0530

    add post model

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-posts = [
-    {
-        "author": "Virendra Singh Rawat",
-        "title": "blog1",
-        "content": "First content",
-        "date_posted": "August 27, 2022"
-    },
-    {
-        "author": "Bhavya",
-        "title": "blog2",
-        "content": "Second content",
-        "date_posted": "August 28, 2022"
-    }
-]
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
