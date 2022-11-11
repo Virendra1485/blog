8a8c5fe2466e65c0aaa3fbbb4abc77fc336c4618
virendra1485 <virendrasinghrawat1485@gmail.com>
2022-11-11 14:06:22 +0530

    add template and css

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,11 +1,26 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+posts = [
+    {
+        "author": "Virendra Singh Rawat",
+        "title": "blog1",
+        "content": "First content",
+        "date_posted": "August 27, 2022"
+    },
+    {
+        "author": "Bhavya",
+        "title": "blog2",
+        "content": "Second content",
+        "date_posted": "August 28, 2022"
+    }
+]
 
 
 def home(request):
-    return HttpResponse("Home Page")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("About Page")
+    return render(request, "blog/about.html", {"title": "Blog"})
